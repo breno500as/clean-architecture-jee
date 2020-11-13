5146286f721674ebe12c3ae104bc72cc279314fa
breno500as <breno500as@gmail.com>
2020-11-13 15:03:05 -0300

    report

--- cleancode-report/src/main/java/com/cleancode/report/CreateReportBookImpl.java
+++ cleancode-report/src/main/java/com/cleancode/report/CreateReportBookImpl.java
@@ -2,15 +2,16 @@ package com.cleancode.report;
 
 import javax.enterprise.context.ApplicationScoped;
 
+import com.cleancode.core.contract.dto.BookDTO;
 import com.cleancode.core.contract.report.CreateReportBook;
- 
 
 @ApplicationScoped
 public class CreateReportBookImpl implements CreateReportBook {
 
 	@Override
-	public void create() {
-		System.out.println("criando relatorio..");	
+	public byte[] create(BookDTO bookDTO) {
+		System.out.println("criando relatorio..");
+		return new byte[] {};
 	}
 
 }
